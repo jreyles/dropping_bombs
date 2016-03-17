0ba95b8b7be940fbd70f9a82b8c1777f13361d03
Kerrin631 <kerrin.arora@gmail.com>
2016-03-17 11:07:05 -0700

    Initial Commit

---
+++ Dropping BombsTests/Dropping_BombsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dropping_BombsTests.swift
+//  Dropping BombsTests
+//
+//  Created by Kerrin Arora on 3/17/16.
+//  Copyright © 2016 Kerrin Arora. All rights reserved.
+//
+
+import XCTest
+@testable import Dropping_Bombs
+
+class Dropping_BombsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
